a4be17891eed86590f6c6be18be80f8b5a4f5cb2
Norbert <Norbert@Laptop_NORBERT>
2016-06-04 13:04:44 +0200

    Merge origin/master

--- src/statki/Plansza.java
+++ src/statki/Plansza.java
@@ -13,9 +13,28 @@ public class Plansza {
   Kratka[][] Pole;
   Statek[] Okret;
   
+  int liczba4_masztowców;
+  int liczba3_masztowców;
+  int liczba2_masztowców;
+  int liczba1_masztowców;
   
-  public Plansza(int rozmiar, int liczba_statkow){
-    this.Pole = new Kratka[rozmiar][rozmiar];
-    this.Okret = new Statek[liczba_statkow];
-  }  
+  
+  public Plansza(int rozmiar,int liczba4_masztowców,int liczba3_masztowców,int liczba2_masztowców,int liczba1_masztowców )
+  {
+      
+            this.liczba1_masztowców=liczba1_masztowców;
+            this.liczba2_masztowców=liczba2_masztowców;
+            this.liczba3_masztowców=liczba3_masztowców;
+            this.liczba4_masztowców=liczba4_masztowców;
+            
+            int liczbaStatkow = liczba4_masztowców+liczba3_masztowców+liczba2_masztowców+liczba1_masztowców;
+  
+            this.Pole = new Kratka[rozmiar][rozmiar];
+            this.Okret = new Statek[liczbaStatkow];
+    
+  } 
+  
+ public void Generuj_ustawienia_statków_na_planszy(){
+     
+ }
 }
