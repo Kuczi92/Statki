5a813d034ef7011022aee1d853be056f0b38c3ef
Norbert <Norbert@Laptop_NORBERT>
2016-06-04 14:38:13 +0200

    Merge origin/master

--- src/statki/Plansza.java
+++ src/statki/Plansza.java
@@ -5,6 +5,8 @@
  */
 package statki;
 
+import java.util.Random;
+
 /**
  *
  * @author Norbert
@@ -17,24 +19,53 @@ public class Plansza {
   int liczba3_masztowców;
   int liczba2_masztowców;
   int liczba1_masztowców;
-  
-  
+  int liczbaStatkow;
+  int rozmiar;
   public Plansza(int rozmiar,int liczba4_masztowców,int liczba3_masztowców,int liczba2_masztowców,int liczba1_masztowców )
-  {
-      
-            this.liczba1_masztowców=liczba1_masztowców;
-            this.liczba2_masztowców=liczba2_masztowców;
-            this.liczba3_masztowców=liczba3_masztowców;
-            this.liczba4_masztowców=liczba4_masztowców;
-            
-            int liczbaStatkow = liczba4_masztowców+liczba3_masztowców+liczba2_masztowców+liczba1_masztowców;
+            {
+                      this.rozmiar=rozmiar;
+                      this.liczba1_masztowców=liczba1_masztowców;
+                      this.liczba2_masztowców=liczba2_masztowców;
+                      this.liczba3_masztowców=liczba3_masztowców;
+                      this.liczba4_masztowców=liczba4_masztowców;
+
+                      this.liczbaStatkow = liczba4_masztowców+liczba3_masztowców+liczba2_masztowców+liczba1_masztowców;
+
+                      this.Pole = new Kratka[rozmiar][rozmiar];
+                      this.Okret = new Statek[liczbaStatkow];
+
+            } 
   
-            this.Pole = new Kratka[rozmiar][rozmiar];
-            this.Okret = new Statek[liczbaStatkow];
-    
-  } 
-  
- public void Generuj_ustawienia_statków_na_planszy(){
-     
- }
+  public void Generuj_ustawienia_statków_na_planszy_losowo()
+            {
+                
+                 Random r = new Random();
+                
+                // komputer losowo dodaje 1 masztowce 
+                for(int i = 0; i<liczba1_masztowców;i++ )
+                { 
+                  Pole[r.nextInt(rozmiar)][r.nextInt(rozmiar)].setKratka(true);
+                 
+                }
+                
+                // komputer losowo dodaje 2 masztowce
+                for(int i = 0; i<liczba2_masztowców;i++ )
+                {  
+                   r.nextInt(rozmiar);
+                   
+                }
+                
+                // komputer losowo dodaje 3 masztowce
+                for(int i = 0; i<liczba3_masztowców;i++ )
+                {  
+                    r.nextInt(rozmiar);
+                }
+                
+                // komputer losowo dodaje 4 masztowce
+                for(int i = 0; i<liczba4_masztowców;i++ )
+                { 
+                      r.nextInt(rozmiar);
+                }
+                
+            }
 }
