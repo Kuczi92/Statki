1db78ba96b441c9cd10ae6429645c3c8eb218cde
Norbert <Norbert@Laptop_NORBERT>
2016-06-04 12:29:45 +0200

    Originalne

---
+++ src/statki/Kratka.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statki;
+
+/**
+ ta klasa zawiera w sobie dane dotyczące obiektu kratki która jest  zajęta przez statek bądź nie
+ */
+public class Kratka {
+    boolean pusta = false;
+    
+    public void setKratka(boolean w)
+    {
+        this.pusta=w;
+    }
+    
+    public boolean getKratka(boolean w)
+    {
+        return this.pusta;
+    }
+}
