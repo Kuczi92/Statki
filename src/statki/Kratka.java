759a3d205a46a43e30539a201a3e8f88f3a8e2fb
Michal <Michal@192.168.0.103>
2016-06-05 18:15:25 +0200

    Poprawienie formatowania kodu

--- src/statki/Kratka.java
+++ src/statki/Kratka.java
@@ -6,35 +6,36 @@
 package statki;
 
 /**
- ta klasa zawiera w sobie dane dotyczące obiektu kratki która jest  zajęta przez statek bądź nie
+ * ta klasa zawiera w sobie dane dotyczące obiektu kratki która jest zajęta
+ * przez statek bądź nie
  */
 public class Kratka
 {
+
     private boolean statek;
     private boolean odwiedzona;
-    
+
     public Kratka(boolean value)
     {
         this.statek = value;
         this.odwiedzona = false;
     }
-    
-    
+
     public void setStatek(boolean value)
     {
         this.statek = value;
     }
-    
+
     public boolean getStatek()
     {
         return this.statek;
     }
-    
+
     public void setOdwiedzona(boolean value)
     {
         this.odwiedzona = value;
     }
-    
+
     public boolean getOdwiedzona()
     {
         return this.odwiedzona;
